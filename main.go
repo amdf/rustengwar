13951cd56c386a0934705f37afe3675698cbc871
amdf <787535+amdf@users.noreply.github.com>
2021-08-02 16:56:55 +0300

    replacements config

--- main.go
+++ main.go
@@ -1,7 +1,17 @@
 package rustengwar
 
+import _ "embed" //embed?
+
+//go:embed conv/default.json
+var defaultConvJSON []byte
+
+type defaultConversion struct {
+	Replacements [][]string `json:"Replacements"`
+}
+
 //Convert russian string to tengwar string
 func Convert(russian string) (tengwar string) {
 	tengwar = russian //to be continued...
+	//TODO: tengwar = strings.Replace(tengwar, "old", "new", -1)
 	return
 }
--- main_test.go
+++ main_test.go
@@ -1,11 +1,22 @@
 package rustengwar
 
-import "testing"
+import (
+	"encoding/json"
+	"testing"
+)
 
 func TestConvert(t *testing.T) {
 	russian := "русский текст"
 	tengwar := Convert(russian)
 	if russian != tengwar {
 		t.Error("convert error")
+	}
+}
+
+func TestLoadReplacements(t *testing.T) {
+	var v defaultConversion
+	err := json.Unmarshal(defaultConvJSON, &v)
+	if err != nil {
+		t.Error("Replacements json Unmarshal error")
 	}
 }
